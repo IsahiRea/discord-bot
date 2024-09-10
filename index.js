7e1a87557ad4417774c7860c4a5c5d29d4b760bf
IsahiRea <Isahi.rea@live.com>
2024-09-10 18:37:52 -0500

    Added tutorial cooldowns & reload command

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -30,35 +31,21 @@ for(const folder of commandFolders) {
     }
 }
 
-// When the client is ready, run this code (only once).
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// Recieve commands (Slash Only)
-client.on(Events.InteractionCreate, async interaction => {
-    if(!interaction.isChatInputCommand()) return;
+for(const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
 
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if(!command) {
-        console.error(`No command matching ${interaction.commandName}.`)
-        return;
+    if(event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
     }
-    
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-
-        if(interaction.replied || interaction.deferred) {
-            await interaction.followUp({content: 'There was an error while executing this command!', ephemeral: true});
-        }
-        else {
-            await interaction.reply({content: 'There was an error while executing this comand!', ephemeral: true});
-        }
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
+
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN);
