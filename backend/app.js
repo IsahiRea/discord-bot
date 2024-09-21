e032ffe06d0d4b4766fed5a63c79dd808bcaf1c8
IsahiRea <Isahi.rea@live.com>
2024-09-20 22:31:07 -0500

    Implemented Oauth plus mongodb

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,35 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 require('./config/passportConfig');
-const apiRoutes = require('./routes/api')
+const apiRoutes = require('./routes/api');
+const connectDB = require('./config/connectDB');
+const MongoStore = require('connect-mongo');
 require('dotenv').config()
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(session({ secret: `${process.env.SECRET}`, resave: false, saveUninitialized: false }));
+app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+
+// Session setup (using connect-mongo for session store)
+app.use(session({
+  secret: process.env.SESSION_SECRET,  // Keep this secret safe (e.g., in .env)
+  resave: false,                       // Prevent session resaving if unmodified
+  saveUninitialized: false,            // Prevent saving uninitialized sessions
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,   // MongoDB connection URI
+    collectionName: 'sessions'         // Name of the collection where sessions will be stored
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,   // 1-week session expiration
+  }
+}));
+
+
+// Passport setup
 app.use(passport.initialize());
 app.use(passport.session());
 
